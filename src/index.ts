fd9e769d754d6040f472a58b6535bcec285b643d
valpar <valmar.parts@gmail.com>
2022-01-02 18:33:49 +0200

    teine video tehtud

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,78 @@
 import express, { Request, Response, Express } from 'express';
-
+import { nanoid } from 'nanoid';
 const app: Express = express();
+
 const port: number = 3000;
 
+interface Movie {
+    id: string;
+    title: string;
+    description: string;
+    author: string;
+}
+
+interface Db {
+  movies: Movie[];
+}
+
+const db: Db = {
+    movies:[
+        {
+            id: 'qwert',
+            title: 'Matrix',
+            description: 'Super lahe film.',
+            author: 'Valmar'
+        },
+        {
+            id: 'asdf',
+            title: 'Avengers',
+            description: 'tõsiselt vinge film.',
+            author: 'Karl'
+        },
+    ],
+};
+
+app.use(express.json());
+
 app.get('/', (req: Request, res: Response) => {
-  res.status(200).json({
+  return res.status(200).json({
     message: 'Alive',
   });
+});
+
+app.get('/movies', (req: Request, res: Response) => {
+  const {movies} = db;
+    return res.status(200).json({
+      movies,
+    });
+});
+
+app.get('/movies/:id', (req: Request, res: Response) => {
+  const {id} = req.params;
+  const movie = db.movies.find((element: Movie) => element.id === id);
+  if(!movie){
+    return res.status(400).json({
+      message: `No movie exists with id: ${id}`,
+    });
+  }
+    return res.status(200).json({
+      movie,
+    });
+});
+
+app.post('/movies', (req: Request, res: Response) => {
+  const { title, description, author} = req.body;
+  const id = nanoid();
+  const movie: Movie = {
+    id,
+    title,
+    description,
+    author
+  };
+  db.movies.push(movie);
+   return res.status(200).json({
+      id: movie.id,
+    });
 });
 
 app.listen(port, () => {
